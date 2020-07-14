9c2a7035cf6cc766a6442631accbf19b2c2e8ebb
Marina Krivova <mg.krivova@gmail.com>
2020-07-14 18:40:36 +0100

    CV parameters tuning

--- short_notebooks/6_parameter_tuning_all.py
+++ short_notebooks/6_parameter_tuning_all.py
@@ -3,20 +3,26 @@
 
 import pandas as pd
 import numpy as np
-import os
 import time
+from sklearn.metrics import r2_score
+from scipy.optimize import curve_fit
+from scipy.optimize import leastsq
+import scipy.optimize as opt
 from tqdm import tqdm
 import warnings
 warnings.filterwarnings("ignore")
-# from parameter_tuning_CV import RunCrossValidation, TestTunedKernels
+
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+import gc
 
 from sklearn.model_selection import LeaveOneOut
-from IPython.display import display
-
 import os
 from data_preprocessing import FilteringCurves, ShowResponseCurves
 from fitting_curves import FittingColumn, ShowResponseCurvesWithFitting, compute_r2_score
-_FOLDER = "./results/"
+_FOLDER = "/home/acq18mk/master/results/"
 
 ### Coding Part
 
